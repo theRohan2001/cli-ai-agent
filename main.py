24cc9c330acc54d81b1cadf76eeb0c748e25de6f
theRohan2001 <rohanwadhwa2001@gmail.com>
2025-12-19 20:09:25 +0000

    feat: verbose output

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,17 +11,22 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="ai-agent")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", "-v", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
 if response.usage_metadata:
     prompt_token = response.usage_metadata.prompt_token_count
     response_token = response.usage_metadata.candidates_token_count
 else:
     raise RuntimeError("No response from the model")
 
-print(f"User prompt: {args.user_prompt}")
-print(f"Prompt tokens: {prompt_token}")
-print(f"Response tokens: {response_token}")
-print(f"Response: \n{response.text}")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {prompt_token}")
+    print(f"Response tokens: {response_token}")
+    print(f"Response: \n{response.text}")
+else:
+    print(f"Response: \n{response.text}")

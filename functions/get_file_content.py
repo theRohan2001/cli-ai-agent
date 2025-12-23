deb8cf68e647d3f2282adeb0e7e7349a2294509b
theRohan2001 <rohanwadhwa2001@gmail.com>
2025-12-23 09:39:52 +0000

    feat: add a function to get file contents

---
+++ functions/get_file_content.py
@@ -0,0 +1,25 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_dir, file_path):
+    try:
+        working_dir_abs = os.path.abspath(working_dir)
+        target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_file_path = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+        valid_file = os.path.isfile(target_file)
+
+        if not valid_file_path:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not valid_file:
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        
+        with open(target_file, "r") as f:
+            content = f.read(MAX_CHARS)
+            if f.read(1):
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return content
+    
+    except Exception as e:
+        return f"Error: {e}"
+    
+
